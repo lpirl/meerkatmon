8d8a1c065f1ab1c9017c62688a44e7995eef4ba9
Lukas Pirl <git@lukas-pirl.de>
2012-04-01 12:34:25 +0200

    added skeleton for MeerkatMon

--- meerkatmon.py
+++ meerkatmon.py
@@ -3,15 +3,41 @@ from urlparse import urlparse, ParseResult
 from cli.app import CommandLineApp
 
 class MeerkatMon(CommandLineApp):
+
+	strategies = list()
+	config = dict()
+
 	def main(self):
 		print "running"
+		self.load_config(self)
+		self.collect_strategies(self)
+		self.run_strategies(self)
+
+	def load_config(self):
+		pass
+
+	def collect_strategies(self):
+		pass
+
+	def run_strategies(self):
+		for strategy in self.strategies:
+			strategy.setup()
+			strategy.do_check()
+			strategy.teardown()
 
 class Strategy:
 	target = None
+
 	def __init__(self, target):
 		if not isinstance(target, ParseResult):
 			TypeError("A Strategy must be initialized with an urlparse.ParseResult")
 		self.target = target
+
+	def setup(self):
+		pass
+
+	def teardown(self):
+		pass
 
 	def target_knowledge(self):
 		"""
@@ -32,5 +58,6 @@ if __name__ == "__main__":
 	monitor = MeerkatMon()
 	monitor.main()
 
-	s = Strategy("8.8.8.8")
-	s.target_knowledge()
+	import strategies
+	print dir(strategies)
+	s2 = strategies.ping.Ping("8.8.8.8")
