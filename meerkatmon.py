5a08cea1bfea09a138f32c906f0a4d7c4750157b
Lukas Pirl <git@lukas-pirl.de>
2012-08-25 17:09:50 +0200

    changed debug method, added defined colors, minor improvements

--- meerkatmon.py
+++ meerkatmon.py
@@ -3,28 +3,33 @@ from urlparse import urlparse, ParseResult
 from inspect import getmembers, isclass
 import strategies as strategies_module
 
+COLOR_STD='\033[0m'
+COLOR_FAIL='\033[31m'
+COLOR_LIGHT='\033[33m'
+
+
+def debug(msg):
+	# TODO: go *args, **kwargs
+	if __debug__:
+		print(msg)
+
 class MeerkatMon():
 
 	default_configs_filename = "./meerkatmon.conf"
 	configs = dict()
 	global_config = dict()
 
-	default_config = {
+	default_global_config = {
 		'target': '',
 		'timeout': '10',
 		'admin': 'root@localhost',
+		'mail_success': 'True',
 	}
 
-	def debug(self, msg):
-		# TODO: go *args, **kwargs
-		if __debug__:
-			print(msg)
-
 	def auto(self):
-		self.debug("started in auto mode")
+		debug("started in auto mode")
 		self.load_configs()
 		self.preprocess_configs()
-		#self.assign_strategies()
 		self.test_targets()
 
 	def load_configs(self, filename = None):
@@ -32,7 +37,7 @@ class MeerkatMon():
 		Method loads configuration from file into dicts.
 		"""
 		filename = filename or self.default_configs_filename
-		self.debug("loading configuration from '%s'" % filename)
+		debug("loading configuration from '%s'" % filename)
 		fp = open(filename, "r")
 		configs = dict()
 		options = dict()
@@ -53,11 +58,13 @@ class MeerkatMon():
 				continue
 
 		fp.close()
-		self.debug("configuration is: '%s'" % unicode(configs))
+		debug("configuration is: '%s'" % unicode(configs))
 
-		global_config = configs.get('global', self.default_config)
+		global_config = configs.get('global', dict())
 		configs.pop('global', None)
+		global_config.update(self.default_global_config)
 
+		self.global_config = global_config
 		self.configs = configs
 
 	def preprocess_configs(self):
@@ -65,7 +72,7 @@ class MeerkatMon():
 		Method prepares every service in configs for monitoring.
 		"""
 		for section, options in self.configs.iteritems():
-			self.debug("processing service '%s'" % section)
+			debug("processing service '%s'" % section)
 			options = self.parse_target(section, options)
 			options = self.assign_strategy(section, options)
 
@@ -75,14 +82,14 @@ class MeerkatMon():
 		Raises if not possible.
 		"""
 		target_str = options['target']
-		self.debug("  parsing target '%s'" % target_str)
+		debug("  parsing target '%s'" % target_str)
 
 		if "//" not in target_str:
 			target_str = "//" + target_str
 
 		parsed_target = urlparse(target_str)
 		options['parsed_target'] = parsed_target
-		self.debug("    " + str(parsed_target))
+		debug("    " + str(parsed_target))
 
 		return options
 
@@ -93,14 +100,14 @@ class MeerkatMon():
 		if not getattr(self, '_strategies', None):
 			self._strategies = [	t[1] for t in
 									getmembers(strategies_module, isclass) ]
-			self.debug("found stategies: %s" % unicode(self._strategies))
+			debug("found stategies: %s" % unicode(self._strategies))
 		return self._strategies
 
 	def assign_strategy(self, section, options):
 		"""
 		Rates all strategies for all targets and selects the best one.
 		"""
-		self.debug("  Searching strategy")
+		debug("  Searching strategy")
 		best_strategy = (None, KNOWLEDGE_NONE)
 		for strategy in self.get_strategies():
 			strategy_for_target = 	strategy(
@@ -110,13 +117,16 @@ class MeerkatMon():
 			knowledge = strategy_for_target.target_knowledge()
 			if knowledge > best_strategy[1]:
 				best_strategy = (strategy_for_target, knowledge)
-		self.debug("    found '%s'" % strategy.__name__)
+
+		debug("    found '%s'" % strategy.__name__)
+		options['strategy'] = best_strategy[0]
 
 		return options
 
 	def test_targets(self):
-		# TODO
-		pass
+		for section, options in self.configs.iteritems():
+			debug("do check for %s" % section)
+			result = options['strategy'].do_check()
 
 KNOWLEDGE_NONE = 0
 KNOWLEDGE_EXISTS = 10
@@ -127,12 +137,13 @@ KNOWLEDGE_FULL = 100
 class Strategy:
 	target = None
 
-	def __init__(self, target, configs=None):
+	def __init__(self, target, options=None):
 		if not isinstance(target, ParseResult):
 			TypeError(
 				"A Strategy must be initialized with an urlparse.ParseResult"
 			)
 		self.target = target
+		self.options = options or dict()
 
 	def setup(self):
 		pass
@@ -170,6 +181,8 @@ class Strategy:
 		raise NotImplementedError(
 			"Subclass %s must provide do_check()" % self.__class__
 		)
+
+	# TODO: add get_mail_body(), get_mail_subject()
 
 if __name__ == "__main__":
 	monitor = MeerkatMon()
