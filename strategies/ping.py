4db25d8acc99b6b91b67cc0432685a33df97c193
Lukas Pirl <git@lukas-pirl.de>
2012-08-25 17:10:39 +0200

    implemented basic ping strategy

--- strategies/ping.py
+++ strategies/ping.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+from subprocess import check_output, CalledProcessError, STDOUT
 
-from meerkatmon import Strategy, KNOWLEDGE_EXISTS, KNOWLEDGE_NONE
+from meerkatmon import (	Strategy,
+							debug,
+							KNOWLEDGE_EXISTS,
+							KNOWLEDGE_NONE,
+							COLOR_LIGHT,
+							COLOR_STD )
 
 class Ping(Strategy):
 
@@ -8,3 +14,23 @@ class Ping(Strategy):
 		if self.target.netloc:
 			return KNOWLEDGE_EXISTS
 		return KNOWLEDGE_NONE
+
+	def do_check(self):
+		cmd = [
+			'/bin/ping',
+			'-W', self.options.get('timeout', '5'),
+			'-c', self.options.get('count', '1'),
+			self.target.netloc
+		]
+
+		debug("running command: %s" % unicode(cmd))
+
+		success = True
+		try:
+			output = check_output(cmd, stderr=STDOUT)
+		except CalledProcessError, e:
+			success = False
+			output = e.output
+		output = ''.join([COLOR_LIGHT, output.strip(), COLOR_STD])
+
+		debug("  had %ssuccess '%s'" % ('NO ' if not success else '', output))
