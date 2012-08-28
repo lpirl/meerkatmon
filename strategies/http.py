0a24dcbd6f2cc074b9b122688ce59a8f36dac2e8
Lukas Pirl <git@lukas-pirl.de>
2012-08-28 21:23:12 +0200

    added basic strategy for checks against

---
+++ strategies/http.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from meerkatmon import (	Strategy,
+							debug,
+							KNOWLEDGE_ALIVE,
+							KNOWLEDGE_NONE,
+							COLOR_LIGHT,
+							COLOR_STD )
+
+class Http(Strategy):
+
+	def target_knowledge(self):
+		if self.target.scheme.lower() in ("http", "https", ):
+			return KNOWLEDGE_ALIVE
+		return KNOWLEDGE_NONE
+
+	def do_check(self):
+		try:
+			response = urlopen(
+				self.target.geturl(),
+				timeout = int(self.options.get('timeout', '5'))
+			)
+		except HTTPError as e:
+			response = e
+
+		self.output = response.msg
+		self.success = response.code == 200
+
+		debug("  had %ssuccess \n\n'%s'\n" % (
+			'NO ' if not self.success else '',
+			''.join([COLOR_LIGHT, self.output, COLOR_STD])
+		))
+
+	def get_mail_message(self):
+		return '\n'.join([
+			self.get_mail_subject(),
+			"",
+			" Output ".center(30, '-'),
+			self.output,
+			"".center(30, '-')
+		])
+
+	def get_mail_subject(self):
+		return "%s getting '%s'!" % (
+			'Success' if self.success else 'Error',
+			self.target.netloc
+		)
+
+	def get_last_check_success(self):
+		try:
+			return self.success
+		except AttributeError:
+			raise RuntimeError(
+				"strategy asked for success prior callind do_check()"
+			)
