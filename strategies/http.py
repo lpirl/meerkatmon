51b9f82b3db10889f2ca87e454a5cc7984a41ba9
Lukas Pirl <git@lukas-pirl.de>
2012-09-24 15:31:55 +0200

    added basic facility to compare results from checks (actual comparison not implemented yet)

--- strategies/http.py
+++ strategies/http.py
@@ -10,47 +10,105 @@ from meerkatmon import (	BaseStrategy,
 
 class Http(BaseStrategy):
 
+	message = None
+	success = False
+	response_str = None
+
 	def target_knowledge(self):
 		if self.target.scheme.lower() in ("http", "https", ):
 			return KNOWLEDGE_ALIVE
 		return KNOWLEDGE_NONE
 
-	def do_check(self):
+	def _do_request(self):
+		"""
+		Method does actually speak with the target and sets
+		self.{message, success, response_str} accordingly.
+		"""
+		response_str = None
 		try:
 			try:
 				response = urlopen(
 					self.target.geturl(),
 					timeout = int(self.options.get('timeout', '5'))
 				)
+				response_str = response.read()
 			except HTTPError as e:
 				response = e
 
-			self.output = response.msg
-			self.success = response.code == 200
+			message = "server said: " + response.msg
+			success = response.code == 200
 
 		except URLError as e:
-			self.success = False
-			self.output = str(e.reason)
+			success = False
+			message = str(e.reason)
 
-		debug("  had %ssuccess \n\n'%s'\n" % (
-			'NO ' if not self.success else '',
-			''.join([COLOR_LIGHT, self.output, COLOR_STD])
+		self.message = message
+		self.success = success
+		self.response_str = response_str
+
+
+	def do_check(self):
+		"""
+		Method coordinates check.
+		"""
+		self._do_request()
+
+		debug("  %sreached\n\n'%s'\n" % (
+			'NOT ' if not self.success else '',
+			''.join([COLOR_LIGHT, self.message, COLOR_STD])
 		))
 
+		if self.success:
+			response_str = self.response_str
+			prev_response_str = self.load_sample()
+			if prev_response_str is not None:
+				self.compare_responses(response_str, prev_response_str)
+			self.save_sample(response_str)
+
+	def compare_responses(self, response1, response2):
+
+		try:
+			max_distance = float(
+				self.options.get('max_distance', None)
+			)
+		except TypeError as e:
+			return
+
+		response1 = str(response1)
+		response2 = str(response2)
+		for junk_character in [' ', '\t']:
+			response1 = response1.replace(junk_character, '')
+			response2 = response2.replace(junk_character, '')
+
+		distance = 0 # implementation needed
+
+		additional_message = "Deviation ratio %f (max %f)" % (
+			distance, max_distance
+		)
+
+		debug("  " + additional_message)
+
+		self.success = distance > max_distance
+		self.message += "\n\n" + additional_message
+
 	def get_mail_message(self):
-		return '\n'.join([
-			self.get_mail_subject(),
-			"",
-			" Output ".center(30, '-'),
-			self.output,
-			"".center(30, '-')
-		])
+		try:
+			return self.message
+		except AttributeError:
+			raise RuntimeError(
+				"strategy asked for check result prior callind do_check()"
+			)
 
 	def get_mail_subject(self):
-		return "%s getting '%s'!" % (
-			'Success' if self.success else 'Error',
-			self.target.netloc
-		)
+		try:
+			return "%s getting '%s'!" % (
+				'Success' if self.success else 'Error',
+				self.target.netloc
+			)
+		except AttributeError:
+			raise RuntimeError(
+				"strategy asked for check result prior callind do_check()"
+			)
 
 	def get_last_check_success(self):
 		try:
